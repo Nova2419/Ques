7e59a7d4934de3193226adf472f54914f5da3078
Nova2419 <shivamojha2419@gmail.com>
2023-08-18 19:26:37 +0530

    Problem 13 Uploaded

---
+++ Problem13.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+contract MicroFinancing {
+    struct FinancialData {
+        uint timestamp;
+        uint amount;
+    }
+
+    mapping(address => FinancialData) public financialRecords;
+
+    function addFinancialData(uint amount) public {
+        FinancialData memory newData = FinancialData(block.timestamp, amount);
+        financialRecords[msg.sender] = newData;
+    }
+
+    function getFinancialData() public view returns (FinancialData memory) {
+        return financialRecords[msg.sender];
+    }
+
+    function updateFinancialData(uint amount) public {
+        financialRecords[msg.sender].amount = amount;
+    }
+    // More funcationalities to be added because this is a major problem in our daily life
+}
